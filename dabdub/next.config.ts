63b5d2adea32228eea9e964bb95967ef9a77397d
xaxxoo <bnahmad83@gmail.com>
2025-12-17 21:36:20 +0100

    next.config.ts edited and fixed

--- dabdub/next.config.ts
+++ dabdub/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const config = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
 
-export default nextConfig;
+export default config({
+  // your existing Next.js config
+});
